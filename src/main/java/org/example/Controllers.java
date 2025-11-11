cc2ccdc50c11d1f379ef63f642a5f6b33f46673a
Petr Puzanov <peta12321@gmail.com>
2025-11-11 17:29:30 +0900

    JAP-01 Hello

--- src/main/java/org/example/Controllers.java
+++ src/main/java/org/example/Controllers.java
@@ -10,4 +10,11 @@ public class Controllers {
     public String sayHello() {
         return "Hello world";
     }
+
+    @GetMapping("/bye")
+    public String sayBye() {
+        return "Bye";
+    }
+
+
 }
